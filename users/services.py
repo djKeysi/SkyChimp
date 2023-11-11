43973bc1fa331775acc6001e8b66cb9a91044329
Зенченко Евгений <zenchenko_es@outlook.com>
2023-11-11 09:37:25 +0700

    добвление почты

--- users/services.py
+++ users/services.py
@@ -7,8 +7,8 @@ def send_verification_code(verification_code, email):
     """Отправляет письмо на почту для верификации почты пользователя"""
     url = reverse('users:verification', args=[verification_code])
     send_mail(
-        subject='Регистрация на Mailsender',
-        message=f'Для регистрации на платформе Mailsender пройдите по ссылке {"http://127.0.0.1:8000" + url}',
+        subject='Регистрация на Mailing',
+        message=f'Для регистрации на платформе Mailing пройдите по ссылке {"http://127.0.0.1:8000" + url}',
         from_email=settings.EMAIL_HOST_USER,
         recipient_list=[email]
     )
